860764e024dbb0c4639b52c7e682fc7e9f96076c
JoelMartinezVilla <JoelMartinezVilla>
2024-01-30 19:24:41 +0100

    Database updated

--- Database/tables.sql
+++ Database/tables.sql
@@ -2,9 +2,22 @@ DROP DATABSE IF EXISTS Bank;
 CREATE DATABASE IF NOT EXISTS Bank;
 USE Bank;
 
-create table accounts(
-    id INT,
-    holder_id VARCHAR(50),
+CREATE TABLE accounts(
+    acc_id INT,
+    holder_id INT,
     balance INT,
-    
+    acc_type VARCHAR(10)
+);
+
+CREATE TABLE users(
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    user_id INT,
+    email_adress VARCHAR(255),
+    telephone_number INT,
+    adress VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+    acc_id INT
 );
